163a99c0e9d9720d2368e8e852487e8db6b0ecb6
CarolMontteiro <carolmonteiro1206@gmail.com>
2024-09-03 12:15:56 -0300

    validando campo pra passar somente números

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
        
+       
 
         function calculate(operation) {
-            var n1 = parseInt(document.querySelector('#n1').value);
-            var n2 = parseInt(document.querySelector('#n2').value);
-            var result = document.getElementById('result');
-            console.log(n1, n2)
-            if (!(n1.value === "") && !(n2.value === "")) {
-            
-                switch (operation){
-                    case 'somar':
-                        console.log('esta operação é de soma');
-                        somar(n1, n2);
-                        break;
-                    case 'subtrair':
-                        console.log('esta operação é de subtrair');
-                        subtrair(n1, n2);
-                        break;
-                    case 'multiplicar':
-                        console.log('esta operação é de multiplicar');
-                        multiplicar(n1, n2);
-                        break;
-                    case 'dividir':
-                        console.log('esta operação é de dividir');
-                        dividir(n1, n2);
-                        break;
-                    default:
-                        console.log('escolha uma operação');
-                } 
-            }else {
-                console.log('Precisa preencher os campos para fazer a operação');
-            }
-               
-        }
+            let n1 = document.getElementById('n1').value;
+            let n2 = document.getElementById('n2').value;
+            let result = document.getElementById('result');
 
+            console.log(n1)
+
+            if(validate(n1, n2)) {
+                
+                n1 = Number(n1);
+                n2 = Number(n2);
+
+                if (!(n1 === "") && !(n2 === "")) {
+            
+                    switch (operation) {
+                        case 'somar':
+                            somar(n1, n2);
+                            break;
+                        case 'subtrair':
+                            subtrair(n1, n2);
+                            break;
+                        case 'multiplicar':
+                            multiplicar(n1, n2);
+                            break;
+                        case 'dividir':
+                            dividir(n1, n2);
+                            break;
+                        default:
+                            alert('escolha uma operação');
+                    } 
+                }else {
+                    alert('Precisa preencher os campos para fazer a operação');
+                }
+            }else {
+                alert('Dados invalidos!');
+            }
+        }
+    
         function somar(n1, n2) {
             result.innerHTML = n1 + n2;
             alert(`A soma entre ${n1} e ${n2} é igual a: ${result.innerHTML}`);
-
-            console.log(result);
-
         }
 
         function subtrair(n1, n2) {
             result.innerHTML = n1 - n2;
             alert(`A subtração entre ${n1} e ${n2} é igual a: ${result.innerHTML}`);
-
-            console.log(result)
         }
 
         function multiplicar(n1, n2) {
             result.innerHTML = n1 * n2;
             alert(`A multiplicação entre ${n1} e ${n2} é igual a: ${result.innerHTML}`);
-
-            console.log(result)
         }
 
         function dividir(n1, n2) {
             result.innerHTML = n1 / n2;
             alert(`A divisão entre ${n1} e ${n2} é igual a: ${result.innerHTML}`);
-
-            console.log(result)
         }
+
+        function validate(value1, value2) {
+            let validate1 = !isNaN(parseFloat(value1)) && !isNaN(isFinite(value1));
+            let validate2 = !isNaN(parseFloat(value2)) && !isNaN(isFinite(value2));
+            return validate1 && validate2;
+
+        }
+
+       
